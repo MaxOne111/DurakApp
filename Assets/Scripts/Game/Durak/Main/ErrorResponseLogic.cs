8b3f44ef2a71ca2087c64bc703396f298014c285
MaxOne <kurdmax012001@inbox.ru>
2025-05-28 00:28:15 +0500

    Bind ErrorResponseLogic

--- Assets/Scripts/Game/Durak/Main/ErrorResponseLogic.cs
+++ Assets/Scripts/Game/Durak/Main/ErrorResponseLogic.cs
@@ -1,6 +1,48 @@
-﻿namespace Game.Max
+﻿using Game.Durak.Enums;
+using Game.Durak.Network.Responses;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class ErrorResponseLogic : IResponse
 {
-    public class ErrorResponseLogic
+    private TestPlayer _player; // Bind ?
+    
+    public void Invoke(string response)
     {
+        ErrorResponse errorResponse = JsonConvert.DeserializeObject<ErrorResponse>(response);
+
+        switch (errorResponse.Error)
+        {
+            case ELogicError.WeakCard:
+                break;
+            case ELogicError.MissingTableCard:
+                //TODO
+                break;
+            case ELogicError.MissingPlayerCard:
+                //TODO
+                break;
+            case ELogicError.OpenSlot:
+                //TODO
+                break;
+            case ELogicError.NotInit:
+                //TODO
+                break;
+            case ELogicError.NotEnemy:
+                //TODO
+                break;
+            case ELogicError.WaitInit:
+                //TODO
+                break;
+            case ELogicError.SlotClosed:
+                //TODO
+                break;
+                    
+            case ELogicError.TournamentRoomNotFound:
+                SceneMediator.Room = errorResponse.next_game;
+                break;
+        }
+
+        if (_player)
+            _player.ReturnSleeveToPlayer();
     }
 }
